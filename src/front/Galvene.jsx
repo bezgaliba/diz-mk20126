63d2e2ae00d44c9f15f2703a4444b84e17e23b10
bezgaliba <mpsamarels@gmail.com>
2023-05-02 14:58:19 +0300

    replaced Trans component and use translation hook

--- src/front/Galvene.jsx
+++ src/front/Galvene.jsx
@@ -6,12 +6,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Trans } from 'react-i18next';
 
 function Galvene() {
+
   const { t } = useTranslation();
-  const onChange = (event) => {
-    i18n.changeLanguage(event.target.value);
+
+  function onLangChange(eventKey){
+    i18n.changeLanguage(eventKey);
+    console.log(eventKey)
   }
   return (
     <>
@@ -23,29 +25,17 @@ function Galvene() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+              <NavDropdown onSelect={onLangChange} title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item eventKey='en'>{t('English')}</NavDropdown.Item>
+                <NavDropdown.Item eventKey='lv'>{t('Latvian')}</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <h1>
-        <Trans i18nKey="uzruna"></Trans>
+        {t('Hello')}
       </h1>
-      <select onChange={onChange}>
-        <option value='en'>English</option>
-        <option value='lv'>Latvian</option>
-      </select>
     </Suspense>
     </>
   );
