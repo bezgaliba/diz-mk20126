e93f2872694e057a7a6bdc2712973494515a2efb
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2023-05-15 12:39:36 +0300

    added for me page section

--- src/front/ParMani.jsx
+++ src/front/ParMani.jsx
@@ -2,6 +2,7 @@ import react, { useRef  } from 'react';
 import './css/ParMani.css';
 import i18n from 'i18next';
 import { useTranslation } from 'react-i18next';
+import es from './resources/me.jpg';
 
 function ParMani() {
 
@@ -9,10 +10,25 @@ function ParMani() {
 
     return (
       <>
-        <div className="myStory">
-          <h1 className="">
-            {t('myStory')}!
-          </h1>
+        <div className="about-me-items">
+          <div className="about-me">
+            <div className="image-grid">
+              <div className="image-container">
+                <img className="es" src={es} alt="es" />
+              </div>
+            </div>
+            <div className="paragraph-grid">
+              <div className="paragraph-header">
+                <p className='paragraph-header-top'>Chapter 1 - <b>The Journey</b></p><p>sis ir virsraksts vizualizacijai</p>
+              </div>
+              <div className="paragraph-container">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a lorem vitae nunc sollicitudin luctus ac in erat. Aenean euismod magna quis eros laoreet elementum.</p>
+              </div>
+              <div className="paragraph-container">
+                <p>Phasellus tristique dolor vel arcu posuere, eu rutrum quam bibendum. Curabitur vel justo eget mi efficitur malesuada. Duis eget feugiat massa.</p>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
