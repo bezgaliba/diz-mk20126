ba34ba001252b06cbea76329cefca8849f704144
bezgaliba <74833724+bezgaliba@users.noreply.github.com>
2023-05-23 09:17:26 +0300

    adjusted arrows and transition on slideshow

--- src/front/Slaidshovs.jsx
+++ src/front/Slaidshovs.jsx
@@ -1,48 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import './css/Slaidshovs.css';
 
 const Slaidshovs = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
   const images = [
     {
       src: 'https://fastly.picsum.photos/id/15/2500/1667.jpg?hmac=Lv03D1Y3AsZ9L2tMMC1KQZekBVaQSDc1waqJ54IHvo4',
-      text: 'non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."'
+      text:
+        'non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."',
     },
     {
       src: 'https://fastly.picsum.photos/id/17/2500/1667.jpg?hmac=HD-JrnNUZjFiP2UZQvWcKrgLoC_pc_ouUSWv8kHsJJY',
-      text: 'Image 2'
+      text: 'Image 2',
     },
     {
       src: 'https://fastly.picsum.photos/id/21/3008/2008.jpg?hmac=T8DSVNvP-QldCew7WD4jj_S3mWwxZPqdF0CNPksSko4',
-      text: 'Image 3'
+      text: 'Image 3',
     },
     {
       src: 'https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA',
-      text: 'Image 4'
-    }
+      text: 'Image 4',
+    },
   ];
 
   const handlePrev = () => {
-    setCurrentIndex(prevIndex => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    if (!isTransitioning) {
+      setIsTransitioning(true);
+      setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    }
   };
 
   const handleNext = () => {
-    setCurrentIndex(prevIndex => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    if (!isTransitioning) {
+      setIsTransitioning(true);
+      setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    }
   };
+
+  useEffect(() => {
+    // Reset transition state after the slide transition is complete
+    const transitionTimeout = setTimeout(() => {
+      setIsTransitioning(false);
+    }, 500); // Adjust the timeout value to match the transition duration in CSS
+
+    return () => clearTimeout(transitionTimeout);
+  }, [currentIndex]);
 
   return (
     <div className="slideshow">
-      <div className="slide">
-        <img className="slideimg" src={images[currentIndex].src} alt={`Slide ${currentIndex}`} />
-        <div className="highlighted-text">{images[currentIndex].text}</div>
-      </div>
+      {images.map((image, index) => (
+        <div className={`slide ${index === currentIndex ? 'active' : ''}`} key={index}>
+          <img className="slideimg" src={image.src} alt={`Slide ${index}`} />
+          <div className="highlighted-text">{image.text}</div>
+        </div>
+      ))}
       <div className="navigation">
         <button className="arrow prev" onClick={handlePrev}>
-          &lt;
+          <FontAwesomeIcon icon={faChevronLeft} />
         </button>
         <button className="arrow next" onClick={handleNext}>
-          &gt;
+          <FontAwesomeIcon icon={faChevronRight} />
         </button>
       </div>
     </div>
